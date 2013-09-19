cd574df0bbe8375aeb8222019ecd3d4ed1b8b0ee
Aniket Pant <me@aniketpant.com>
2013-09-19 21:39:43 +0530

    Update code for using as a jQuery plugin
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- src/immoral.js
+++ src/immoral.js
@@ -9,30 +9,91 @@
 (function($) {
 
   // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  $.fn.immoral = function() {
+    return this.each(function() {
+      // Do something immoral to each selected element.
+      init($(this).attr('href'));
+
+      $('.modal').on('click', 'a[rel="modal:close"]', function(e) {
+        e.preventDefault();
+        var parentModal = '#' + $(this).closest('.modal').attr('id');
+        modalClose(parentModal);
+      });
     });
   };
 
   // Static method.
-  $.awesome = function(options) {
+  $.immoral = function(options) {
     // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
+    $.immoral.options = $.extend({}, $.immoral.options, options);
+    // Return something immoral.
+    return 'immoralized';
   };
+
+  function modalShadowInit() {
+    var modalShadow = $.immoral.options.modalShadow;
+    if(!modalShadow) {
+      $('body').append($.immoral.options.modalShadowDiv);
+      $.immoral.options.modalShadow = $('#modal_shadow');
+    }
+  }
+
+  function init(element) {
+    var modalObj = $(element);
+    var modalObjName = modalObj.attr('id');
+    modalObj.wrap($.immoral.options.modalWrapper);
+    var modalWhole = modalObj.parent('.modal-wrapper');
+
+    // Hide the whole thing.
+    modalWhole.hide().attr('id', modalObjName + '-wrapper');
+
+    // apply styles
+    modalObj.css($.immoral.options.modalStyle).show();
+
+    // Add close Button
+    $($.immoral.options.modalCloseButton).attr('id', modalObjName + '_close');
+    modalObj.prepend($.immoral.options.modalCloseButton);
+
+    // Initialize Modal Shadow
+    modalShadowInit();
+    modalOn(element);
+  }
+
+  function modalOn(element) {
+    var modalWhole = $(element).parent();
+    var modalShadow = $.immoral.options.modalShadow;
+    modalShadow.fadeIn();
+    modalWhole.fadeIn();
+  }
+
+  function modalClose(element) {
+    if(element) {
+      var modalWhole = $(element).parent('.modal-wrapper');
+      modalWhole.fadeOut();
+    } else {
+      $('.modal').parent().fadeOut();
+    }
+    var modalShadow = $.immoral.options.modalShadow;
+    modalShadow.fadeOut();
+  }
 
   // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
+  $.immoral.options = {
+    modalShadowDiv: '<div id="modal_shadow" style="display:none;"></div>',
+    modalWrapper: '<div class="modal-wrapper" />',
+    modalCloseButton: '<a href="#" rel="modal:close">Close</a>',
+    modalShadow: false,
+    modalStyle: {
+      width: '50%',
+      height: '50%',
+      margin: '0 auto'
+    }
   };
 
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
+  // Custom element.
+  $.expr[':'].immoral = function(elem) {
+    // Is this element immoral?
+    return $(elem).text().indexOf('immoral') !== -1;
   };
 
 }(jQuery));
